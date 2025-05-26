5c0e83b926cc7b4c55015467d080dd573c3ff5b7
Vivek Pokharankar <vivek00088888@gmal.com>
2025-05-27 01:32:41 +0530

    udpate ui and posting

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import CreatePost from '../components/CreatePost';
+import axios from "axios";
 
 const Home = () => {
+const createPost = async(data) => {
+  setError("")
+  try {
+    const response = axios.post(`${import.meta.env.VITE_Backend_api_url}/posts`, data, {
+      withCredentials: true,
+      headers : {
+        Authorization : `Bearer ${token}`
+      }, 
+    })
+
+    o
+  }catch(err) {
+    setError(err.response.data.message)
+
+  }
+}
+
+
+
+
+  const [posts,setPosts] = useState([]);
+  const [loading,isLoading] = useState(false);
+  const[error,setError] = useState("")
+  const token = useSelector(state=> state?.user?.currentUser?.token)
+
   return (
-    <div>
-      Home
-    </div>
+    <section className='mainArea'>
+      <CreatePost onCreatePost = {createPost} error={error}></CreatePost>
+    </section>   
   )
 }
 
